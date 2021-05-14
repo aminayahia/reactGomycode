1790378edd216d524e5c545ec510d08b6c23477d
aminayahia <a.yahia@pixelstrade.com>
2021-05-14 17:25:44 +0100

    REACT JS Checkpoint

---
+++ src/App.js
@@ -0,0 +1,30 @@
+//import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form className="p-3 mb-2 bg-light text-dark">
+          <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+           
+          </div>
+          <div className="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+          </div>
+          <div className="form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" for="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
